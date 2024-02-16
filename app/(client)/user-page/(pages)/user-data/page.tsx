ad954f8d5a30a118fa4642fdb0ec9d9bc54ea83f
theNamellessOne <145769204+theNamellessOne@users.noreply.github.com>
2024-02-16 16:46:37 +0200

    Update page.tsx

--- app/(client)/user-page/(pages)/user-data/page.tsx
+++ app/(client)/user-page/(pages)/user-data/page.tsx
@@ -1,16 +1,34 @@
-import React from "react";
+"use client";
+
+import { UserService } from "@/service/user/user-service";
+import { useSession } from "next-auth/react";
+import React, { useEffect, useState } from "react";
 
 export default async function UserData() {
-    // const {data: session} = useSession({
-    //     required: true,
-    //     onUnauthenticated() {
-    //         redirect('/login')
-    //     }
-    // })
-    // if (!session?.user) return
-    return (
-        <div className={"px-[20%] pt-[15px] flex"}>
-            user data
+  const session = useSession();
+  const [userData, setUserData] = useState<any>(null);
+
+  useEffect(() => {
+    const id = session.data?.user.id;
+    if (!id) return;
+
+    UserService.instance.fetchById(id).then((res) => {
+      if (res.errMsg) return;
+
+      setUserData(res.value);
+    });
+  }, [session]);
+
+  return (
+    <div className="px-[20%] pt-[15px] flex">
+      {userData && (
+        <div className={"flex flex-col"}>
+          <p>Name: {userData.name}</p>
+          <p>Email: {userData.email}</p>
+          <p>Phone: {userData.phone}</p>
+          <p>Username: {userData.username}</p>
         </div>
-    )
+      )}
+    </div>
+  );
 }
