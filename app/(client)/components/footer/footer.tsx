fc4f5a74abefee869948d8b06125ea67bc29f1c6
theNamellessOne <ighor.pavluuk@gmail.com>
2024-02-20 16:49:03 +0200

    added toast when item added to cart fixed footer not rendering properly on checkout page. might remove footer from that page

--- app/(client)/components/footer/footer.tsx
+++ app/(client)/components/footer/footer.tsx
@@ -5,7 +5,9 @@ import { FooterLogo } from "./footer-logo";
 
 export function Footer() {
   return (
-    <footer className={"bg-secondary py-8 px-4 md:px-8"}>
+    <footer
+      className={"bg-secondary py-8 px-4 md:px-8 border-1 border-t-primary/60"}
+    >
       <div
         className={
           "container mx-auto flex flex-col md:flex-row items-center justify-between gap-4"
