81509fe256239c8f700c1306c5201bad77d49b09
theNamellessONe <ighor.pavluuk@gmail.com>
2024-03-02 18:31:22 +0200

    added image compression

--- app/dashboard/components/ui/file-upload.tsx
+++ app/dashboard/components/ui/file-upload.tsx
@@ -1,5 +1,6 @@
-import { UploadDropzone } from "@/lib/uploadthing";
 import { fileRouter } from "@/app/api/uploadthing/core";
+import { UploadDropzone } from "@/lib/uploadthing";
+import imageCompression from "browser-image-compression";
 import { UploadFileResponse } from "uploadthing/client";
 
 type FileUploadProps = {
@@ -22,6 +23,22 @@ export function FileUpload({
         button: "bg-primary px-4",
       }}
       endpoint={endpoint}
+      onBeforeUploadBegin={async (files) => {
+        const compress = async () => {
+          const options = {
+            maxWidthOrHeight: 1024,
+            useWebWorker: true,
+          };
+
+          let promises = files.map(async (file) => {
+            return await imageCompression(file, options);
+          });
+
+          return await Promise.all(promises);
+        };
+
+        return await compress();
+      }}
       onClientUploadComplete={onUploadComplete}
       onUploadBegin={onUploadBegin}
     />
