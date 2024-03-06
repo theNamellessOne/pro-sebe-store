49bff7df457cbed6332c86ee4840adb83732ebff
theNamellessONe <ighor.pavluuk@gmail.com>
2024-03-06 17:21:30 +0200

    still trying to fix this shit

--- ssl-update.sh
+++ ssl-update.sh
@@ -11,8 +11,7 @@ cd /usr/src/openssl-3.0.0
 
 ./config
 make
-make test
-make install
+make -j$(nproc) build_tests=no install
 ln -s /usr/local/lib64/libssl.so.3 /usr/lib64/libssl.so.3
 ln -s /usr/local/lib64/libcrypto.so.3 /usr/lib64/libcrypto.so.3
 openssl version
