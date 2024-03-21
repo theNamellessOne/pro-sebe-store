ced6775159ba5cbc5a2bd3c41d4f3956c4f9ebf3
theNamellessONe <ighor.pavluuk@gmail.com>
2024-03-21 07:53:57 +0200

    webhook test

--- app/api/payment-confirm/webhook/route.ts
+++ app/api/payment-confirm/webhook/route.ts
@@ -1,6 +1,13 @@
 import { NextRequest } from "next/server";
+import {OrderService} from "@/service/order/order-service";
 
 export async function POST(req: NextRequest) {
-  console.log("---> request headers:", req.headers);
-  console.log("---> request body:", await req.json());
+  const body = await req.json();
+
+  console.log("---> request sign header:", req.headers.get("x-sign"));
+  console.log("---> request body:", body);
+
+  if (body.status === "success") await OrderService.instance.confirmOrder(body.reference);
+
+  return new Response();
 }
