9337c1cf017c850560446a183c64fad08e7f11c3
DmytroOss <dmutrogytsylak@gmail.com>
2024-03-28 00:44:15 +0200

    fixed payment

--- app/api/payment-confirm/webhook/route.ts
+++ app/api/payment-confirm/webhook/route.ts
@@ -1,43 +1,39 @@
 import { NextRequest } from "next/server";
-import {OrderService} from "@/service/order/order-service";
+import { OrderService } from "@/service/order/order-service";
 
 export async function POST(req: NextRequest) {
   const body = await req.json();
-  const sign = req.headers.get("x-sign")
+  const sign = req.headers.get("x-sign");
 
   console.log("---> request sign header:", sign);
   console.log("---> request body:", body);
 
   if (!sign) return new Response();
 
-  let resp =  await (await fetch("https://api.monobank.ua/api/merchant/pubkey", {
+  const resp = await (await fetch("https://api.monobank.ua/api/merchant/pubkey", {
     headers: {
       'X-Token': process.env.MONOBANK_API_KEY!
     }
-  })).json()
+  })).json();
 
   const crypto = require('crypto');
 
+  const message = JSON.stringify(body);
 
-  let message = body.string;
+  const signatureBuf = Buffer.from(sign, 'base64');
+  const publicKeyBuf = Buffer.from(resp.key, 'base64');
 
-  let signatureBuf = Buffer.from(sign, 'base64');
-  let publicKeyBuf = Buffer.from(resp, 'base64');
+  const verify = crypto.createVerify('sha256');
 
-  let verify = crypto.createVerify('SHA256');
+  verify.update(message);
 
-  verify.write(message);
-  verify.end();
+  const result = verify.verify(publicKeyBuf, signatureBuf);
 
-  let result = verify.verify(publicKeyBuf, signatureBuf);
-
-  if(result){
+  if (result) {
     if (body.status === "success") await OrderService.instance.confirmOrder(body.reference);
-  }else{
-    console.log("blyat")
+  } else {
+    console.log("blyat");
   }
-
-
 
   return new Response();
 }
