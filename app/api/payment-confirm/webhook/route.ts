f65f8f0dca9a89cdf7c2664629329f3f88ceaae7
DmytroOss <dmutrogytsylak@gmail.com>
2024-03-28 02:25:08 +0200

    idk what is it

--- app/api/payment-confirm/webhook/route.ts
+++ app/api/payment-confirm/webhook/route.ts
@@ -21,7 +21,7 @@ export async function POST(req: NextRequest) {
   const message = JSON.stringify(body);
 
   const signatureBuf = Buffer.from(sign, 'base64');
-  const publicKeyBuf = Buffer.from(resp.key, 'base64');
+  const publicKeyBuf = Buffer.from(process.env.MONO_KEY!, 'base64');
 
   const verify = crypto.createVerify('sha256');
 
@@ -32,7 +32,7 @@ export async function POST(req: NextRequest) {
   if (result) {
     if (body.status === "success") await OrderService.instance.confirmOrder(body.reference);
   } else {
-    console.log("blyat");
+    console.log(result);
   }
 
   return new Response();
